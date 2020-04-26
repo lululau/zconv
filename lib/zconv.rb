9f7144480d753e6a13477c2ff8c4735c05d96e87
Liu Xiang <liuxiang@ktjr.com>
2020-04-26 22:24:17 +0800

    Initial Commit

---
+++ lib/zconv.rb
@@ -0,0 +1,5 @@
+require "zconv/version"
+require "zconv/app"
+
+module Zconv
+end
---
+++ spec/zconv_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Zconv do
+  it "has a version number" do
+    expect(Zconv::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
